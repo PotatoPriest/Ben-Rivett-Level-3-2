f22e1c3164ee6f72081e8373f107c1225635deba
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2024-02-18 21:46:36 +0000

    Creating the test branch and adding a definition for labels and buttons

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Hello world")
-window.geometry("300x300")
+def label(text):
+    label = tk.Label(text=text)
+    label.pack()
+    
+def button(text, command):
+    button = tk.Button(text=text, command=command)
+    button.pack()
 
-hello = tk.Label(text="Hello world!")
-hello.pack()
-button = tk.Button(text="Click me!")
-button.pack()
+class main:
+    def __init__(self):
+        window = tk.Tk()
+        window.title("Game by: Benjamin Rivett")
+        window.geometry("300x300")
+        label("This is a label")
+        button("This is a button", None)
+        tk.mainloop()
 
-tk.mainloop()
+
+
+main()
