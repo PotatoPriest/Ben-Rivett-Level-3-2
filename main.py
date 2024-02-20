15f1f61b0cd7bcbfe4b6456a6184bc24ba42263a
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2024-02-20 00:50:22 +0000

    created a error definition and changed the way the programme is started

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
 
-def label(text):
+def label(text): # This definition is used to create a label
     label = tk.Label(text=text)
     label.pack()
     
-def button(text, command):
+def button(text, command): # This definition is used to create a button
     button = tk.Button(text=text, command=command)
     button.pack()
 
-class main:
+def Error(): # This definition is used to error catch
+    messagebox.showerror(title="Error", message="There has been an error")
+
+class window: # This class is used to create the window of the programme
     def __init__(self):
-        window = tk.Tk()
-        window.title("Game by: Benjamin Rivett")
-        window.geometry("300x300")
+        self.window = tk.Tk()
+        self.window.title("Game by: Benjamin Rivett")
+        self.window.geometry("400x300")
         label("This is a label")
-        button("This is a button", None)
-        tk.mainloop()
+        button("This is a button", Error)
 
 
 
-main()
+main = window() # This calls the window class
+main.window.mainloop() # This runs the window
