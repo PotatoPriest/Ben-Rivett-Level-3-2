ddfe44469a6c061c0a9ebd9618fda523f83eecce
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2024-05-23 00:20:39 +0000

    Basic Menu system established and added a way to change the colours of the programme

--- main.py
+++ main.py
@@ -2,19 +2,20 @@
 # Date - 20/2/2024
 # Version - 0.1
 # This is the most basic version of this programme.
+# State guide : 0 = Main Menu, 1 = Settings
 
 # Importing required things
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import simpledialog
+from tkinter import messagebox, simpledialog
+base_colour = "#d9d9d9"
 
-def label(text): # This definition is used to create a label
-    label = tk.Label(text=text)
-    label.pack()
+def label(master, background, foreground, text): # This definition is used to create a label
+    label = tk.Label(master=master, background=background, foreground=foreground, text=text)
+    label.pack(pady=3)
     
-def button(text, command): # This definition is used to create a button
-    button = tk.Button(text=text, command=command)
-    button.pack()
+def button(master, background, foreground, text, command): # This definition is used to create a button
+    button = tk.Button(master=master, background=background, foreground=foreground, text=text, command=command)
+    button.pack(pady=3)
 
 def Error(): # This definition is used to error catch
     messagebox.showerror(title="Error", message="There has been an error")
@@ -22,11 +23,77 @@ def Error(): # This definition is used to error catch
 class window: # This class is used to create the window of the programme
     def __init__(self): # definition for when the class is initilized
         self.window = tk.Tk()
+        self.bg_colour = base_colour
+        self.bt_colour = base_colour
+        self.txt_colour = "#000000"
         self.window.title("Game by: Benjamin Rivett")
         self.window.geometry("400x300")
-        label("This is a label")
-        button("This is a button", Error)
+        self.main_menu()
+        
+    def main_menu(self): # This definition is used to create the main menu
+        self.state = 0
+        self.menu_frame = tk.Frame(self.window, background=self.bg_colour)
+        self.menu_frame.pack()
+        label(self.menu_frame, self.bg_colour, self.txt_colour, "Test words")
+        button(self.menu_frame, self.bt_colour, self.txt_colour, "Play", Error)
+        button(self.menu_frame, self.bt_colour, self.txt_colour, "Settings", self.settings_menu)
+        button(self.menu_frame, self.bt_colour, self.txt_colour, "Exit", self.back)
 
+    def back(self): # definition that allows the user to go back a menu
+        if self.state == 0:
+            if messagebox.askyesno("Exit", "Are you sure you want to exit?"):
+                self.window.destroy()
+            
+        elif self.state == 1:
+            self.settings_frame.destroy()
+            self.main_menu()
+            
+    def settings_menu(self): # this is the settings menu
+        self.state = 1
+        self.menu_frame.destroy()
+        self.settings_frame = tk.Frame(self.window, background=self.bg_colour)
+        self.settings_frame.pack()
+        label(self.settings_frame, self.bg_colour, self.txt_colour, "Settings")
+        label(self.settings_frame, self.bg_colour, self.txt_colour, "Accessibility Options:")
+        button(self.settings_frame, self.bt_colour, self.txt_colour, "Background Colour", lambda: self.colour_picker("background"))
+        button(self.settings_frame, self.bt_colour, self.txt_colour, "Text Colour", lambda: self.colour_picker("text"))
+        button(self.settings_frame, self.bt_colour, self.txt_colour, "Button Colour", lambda: self.colour_picker("button"))
+        button(self.settings_frame, self.bt_colour, self.txt_colour, "Back", self.back)
+
+    def colour_picker(self, state): # this is a popup window that allows the user to pick a background colour
+        self.colour_window = tk.Tk()
+        self.colour_state = state
+        
+        if self.colour_state == "background":
+            self.colour_window.title("Background Colour")
+            label(self.colour_window, self.bg_colour, self.txt_colour, "Pick the background colour")
+            
+        elif self.colour_state == "text":
+            self.colour_window.title("Text Colour")
+            label(self.colour_window, self.bg_colour, self.txt_colour, "Pick the text colour")
+
+        elif self.colour_state == "button":
+            self.colour_window.title("Button Colour")
+            label(self.colour_window, self.bg_colour, self.txt_colour, "Pick the button colour")
+        
+        button(self.colour_window, self.bt_colour, self.txt_colour, "Light Gray", lambda: self.colour_setter(base_colour))
+        button(self.colour_window, self.bt_colour, self.txt_colour, "Black", lambda: self.colour_setter("#000000"))
+        button(self.colour_window, self.bt_colour, self.txt_colour, "White", lambda: self.colour_setter("#ffffff"))
+        button(self.colour_window, self.bt_colour, self.txt_colour, "Red", lambda: self.colour_setter("#ff0000"))
+        button(self.colour_window, self.bt_colour, self.txt_colour, "Green", lambda: self.colour_setter("#00ff00"))
+        button(self.colour_window, self.bt_colour, self.txt_colour, "Blue", lambda: self.colour_setter("#0000ff"))
+
+    def colour_setter(self, colour): # this updates the background colour
+        if self.colour_state == "background":
+            self.bg_colour = colour
+        elif self.colour_state == "text":
+            self.txt_colour = colour
+        elif self.colour_state == "button":
+            self.bt_colour = colour
+            
+        self.settings_frame.destroy()
+        self.settings_menu()
+        self.colour_window.destroy()
 
 
 main = window() # This calls the window class
