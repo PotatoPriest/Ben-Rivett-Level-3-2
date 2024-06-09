7e186cd67d3ef9f55fb2c530767d969eb46cf218
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2024-06-09 22:48:09 +0000

    I worked on the "Learn" area of the game and I worked on the game start screen

--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,14 @@
 # Name - Benjamin Rivett
-# Date - 6/6/2024
-# Version - 0.4
-# This version finishes the custom colour function and addes the colours and score to the saved data
+# Date - 10/6/2024
+# Version - 0.5
+# This version I started to work on the area where the user can learn about A.I
 # State guide : 0 = Main Menu, 1 = Game, 2 = Settings, 3 = Save
 
 # Importing required things
 import tkinter as tk
-from tkinter import Tcl, TclError, messagebox, simpledialog
+from tkinter import TclError, messagebox, simpledialog, StringVar
+
+option_menu = ["Select", "Learn", "Quiz"]
 
 def label(master, background, foreground, text): # This definition is used to create a label
     label = tk.Label(master=master, background=background, foreground=foreground, text=text)
@@ -144,7 +146,7 @@ class window: # This class is used to create the window of the programme
         button(self.colour_frame_right, self.bt_colour, self.txt_colour, "Red", lambda: self.colour_setter("#ff0000"))
         button(self.colour_frame_right, self.bt_colour, self.txt_colour, "Green", lambda: self.colour_setter("#00ff00"))
         button(self.colour_frame_right, self.bt_colour, self.txt_colour, "Blue", lambda: self.colour_setter("#0000ff"))
-        button(self.colour_frame, self.bt_colour, self.txt_colour, "custom", self.custom_colour)
+        button(self.colour_frame, self.bt_colour, self.txt_colour, "Custom", self.custom_colour)
 
     def custom_colour(self): # this definition allows the user to set a custom colour
         self.custom = simpledialog.askstring("Custom Colour", "Enter a hex code for the colour")
@@ -175,12 +177,75 @@ class window: # This class is used to create the window of the programme
         self.settings_menu()
         self.colour_window.destroy()
 
-    def play(self): # this is the game
+    def play(self): # this is the game 
         self.state = 1
+        self.name = None
+        self.game_state_menu = StringVar()
+        self.game_state_menu.set("Select")
         self.menu_frame.destroy()
         self.game_frame = tk.Frame(self.window, background=self.bg_colour)
         self.game_frame.pack(fill="both", expand=True)
+        label(self.game_frame, self.bg_colour, self.txt_colour, """This game is going to teach you about the good and the bad
+things that Artificial Intelligence is capable of.
+Please pick either the Learn or Quiz option.""")
+        self.name_label = tk.Label(self.game_frame, text="Name = {}".format(self.name), background=self.bg_colour, foreground=self.txt_colour)
+        self.name_label.pack()
+        button(self.game_frame, self.bt_colour, self.txt_colour, "Name set", self.name_set)
+        self.game_state = tk.OptionMenu(self.game_frame, self.game_state_menu, *option_menu)
+        self.game_state.pack()
+        button(self.game_frame, self.bt_colour, self.txt_colour, "Start", self.start_game)
         button(self.game_frame, self.bt_colour, self.txt_colour, "Back", self.back)
 
+    def name_set(self):
+        self.name = simpledialog.askstring("Name", "Enter your name")
+        if self.name is None or "":
+            messagebox.showerror("Error", "No name entered")
+        else:
+            self.name_label.config(text="Name = {}".format(self.name))
+            messagebox.showinfo("Succsess", "Your name has been set to {}".format(self.name))
+            
+    def start_game(self): # this is the start of the game
+        self.option = self.game_state_menu.get()
+        if self.option == "Select":
+            messagebox.showerror("Error", "Please pick an option")
+        elif self.option == "Learn":
+            self.game_frame.destroy()
+            self.game_learn()
+        elif self.option == "Quiz":
+            self.game_frame.destroy()
+            self.game_quiz()
+        else:
+            Error()
+
+    def previous_page_def(self):
+        self.content_frame_0.destroy()
+    
+    def game_learn(self):
+        self.page_number = 0
+        self.game_learn_frame = tk.Frame(self.window, background=self.bg_colour)
+        self.game_learn_frame.pack(fill="both", expand=True)
+        self.content_frame_0 = tk.Frame(self.game_learn_frame, background=self.bg_colour)
+        self.content_frame_0.pack()
+        label(self.content_frame_0, self.bg_colour, self.txt_colour, """This section of the game is designed toteach you about
+Artificial Intelligence""")
+        self.button_frame = tk.Frame(self.game_learn_frame, background="green")
+        self.button_frame.pack(anchor="s", fill="both", side="bottom")
+        self.previous_page = tk.Button(self.button_frame, text="Previous Page", command=self.previous_page_def)
+        self.previous_page.pack(side="left")
+        self.next_page = tk.Button(self.button_frame, text="Next Page", command=self.previous_page_def)
+        self.next_page.pack(side="right")
+        self.page()
+    
+    def page(self):
+        if self.page_number <= 0:
+            self.previous_page.config(state="disabled")
+        else:
+            self.previous_page.config(state="active")
+            
+        
+    
+    def game_quiz(self):
+        Error()
+        
 main = window() # This calls the window class
 main.window.mainloop() # This runs the window
