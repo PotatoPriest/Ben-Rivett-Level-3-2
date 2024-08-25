03fdf5974a145aa15225b6de8655b9ee8bec4e70
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2024-08-25 21:36:30 +0000

    I finished the level select feature

--- Game/test.py
+++ Game/test.py
@@ -1,7 +1,7 @@
 # Name - Benjamin Rivett
 # Date - 10/6/2024
 # Version - 0.5
-# In this version I started on a way for the user to select their level.
+# This version allows the user to choose a level.
 # State guide : 0 = Main Menu, 1 = Game, 2 = Settings, 3 = Save, 4 = Level Select, 5 = Game Learn
 
 # Importing required things
@@ -9,7 +9,6 @@ import tkinter as tk
 from contextlib import suppress
 from tkinter import TclError, messagebox, simpledialog
 
-option_menu = ["Select", "Learn", "Quiz"]
 
 def label(master, background, foreground, text): # This definition is used to create a label
     label = tk.Label(master=master, background=background, foreground=foreground, text=text)
@@ -37,6 +36,8 @@ class window: # This class is used to create the window of the programme
         self.score_frame.pack(fill="both")
         self.score_label = tk.Label(self.score_frame, text="Score: " + str(self.score))
         self.score_label.pack(anchor="nw", side = "left")
+        self.level_label = tk.Label(self.score_frame, text="Level: " + str(self.level))
+        self.level_label.pack(anchor="ne", side = "right")
         self.score_button = tk.Button(self.score_frame, text="Score +1", command=self.score_add)
         self.score_button.pack(anchor="nw", side = "left")
         self.level_button = tk.Button(self.score_frame, text="Level +1", command=self.level_add)
@@ -45,9 +46,12 @@ class window: # This class is used to create the window of the programme
         self.dummy_colour_check = tk.Label(self.score_frame, text="")
         self.dummy_colour_check.pack(anchor="ne", side = "right")
 
-    def level_add(self):
-        self.level = int(self.level)
-        self.level += 1
+    def level_add(self): # This function is used to add a level
+        if int(self.level) < 15:
+            self.level = int(self.level)
+            self.level += 1
+        self.update_level()
+
     def score_add(self): # This definition increases the score by 1
         self.score = int(self.score)
         self.score += 1
@@ -57,6 +61,9 @@ class window: # This class is used to create the window of the programme
         self.score_label.config(text="Score: " + str(self.score), foreground=self.txt_colour, background=self.bg_colour)
         self.score_frame.config(background=self.bg_colour)
         self.dummy_colour_check.config(background=self.bg_colour)
+
+    def update_level(self): # This definition is used to update the score
+        self.level_label.config(text="Level: " + str(self.level), foreground=self.txt_colour, background=self.bg_colour)
 
     def main_menu(self): # This definition is used to create the main menu
         self.state = 0
@@ -91,16 +98,17 @@ class window: # This class is used to create the window of the programme
 
         elif self.state == 5: # Game learn state
             self.game_learn_frame.destroy()
+            self.page_label.destroy()
             self.game_menu()
 
         elif self.state == 6: # Game quiz state
             self.level_select_button.destroy()
-            frame_attr = f"level_{level_num}" # I think this might be useless
+            frame_attr = f"level_{level_num}"
             if hasattr(self, frame_attr):
                 getattr(self, frame_attr).destroy()
             self.level_select()
 
-    def save_menu(self): # definition that allows the user to save the game
+    def save_menu(self): # definition that creates the save menu
         self.state = 2
         self.menu_frame.destroy()
         self.save_menu_frame = tk.Frame(self.window, background=self.bg_colour)
@@ -124,13 +132,14 @@ class window: # This class is used to create the window of the programme
             self.txt_colour = self.save_file.readline().strip()
             self.bt_colour = self.save_file.readline().strip()
         self.update_score()
+        self.update_level()
         self.save_menu_frame.destroy()
         self.save_menu()
         messagebox.showinfo("Save Loaded", "Your save has been successfully loaded")
 
     def reset_file_def(self): # Definition for reseting the save file
         with open("Game/save.txt", "w") as self.save_file:
-            self.save_file.writelines("0\n1\n#d9d9d9\n#000000\n#d9d9d9")
+            self.save_file.writelines("0\n0\n#d9d9d9\n#000000\n#d9d9d9")
         messagebox.showinfo("Save Reset", "Your save has been successfully reset")
 
     def settings_menu(self): # this is the settings menu
@@ -217,12 +226,17 @@ class window: # This class is used to create the window of the programme
 
     def level_select(self): # This allows the user to select a level or continue where they left off
         self.state = 4
+        self.btn = 0
         self.game_frame.destroy()
         self.level_select_frame = tk.Frame(self.window, background=self.bg_colour)
         self.level_select_frame.pack(fill="both", expand=True)
-        button(self.level_select_frame, self.bt_colour, self.txt_colour, "Continue", lambda: self.game_start(self.level))
+        self.top_frame = tk.Frame(self.level_select_frame, background=self.bg_colour)
+        self.top_frame.pack()
+        self.continue_button = tk.Button(self.top_frame, text="Continue", command= lambda: self.game_start(self.level))
+        self.continue_button.pack(side="right")
+        self.button_zero = tk.Button(self.top_frame, text="Level 0", command=lambda: self.game_start(0))
+        self.button_zero.pack(side="left")
         if int(self.level) > 0:
-            self.btn = 0
             self.middle_frame = tk.Frame(self.level_select_frame, background=self.bg_colour)
             self.middle_frame.pack()
             if int(self.level) <= 5:
@@ -241,7 +255,7 @@ class window: # This class is used to create the window of the programme
                 self.lnc3.pack(anchor="ne", side="right", padx=2)
                 self.lnc2.pack(anchor="center", padx=2)
             
-            for x in range(int(self.level)):
+            for x in range(1, (int(self.level) +1)):
                 level_num = x
                 self.btn += 1
                 def inner_func(level_num=level_num):
@@ -255,9 +269,6 @@ class window: # This class is used to create the window of the programme
                 elif self.btn <= 15:
                     with suppress(AttributeError):
                         button(self.lnc3, self.bt_colour, self.txt_colour, "Level {}".format(x), inner_func)
-                        
-        else:
-            button(self.level_select_frame, self.bt_colour, self.txt_colour, "Level 0", lambda: self.game_start(0))
             
         button(self.level_select_frame, self.bt_colour, self.txt_colour, "Back", lambda: self.back(None))
 
@@ -269,7 +280,7 @@ class window: # This class is used to create the window of the programme
         if level_num == 0:
             self.level_0 = tk.Frame(self.window, background=self.bg_colour)
             self.level_0.pack(fill="both", expand=True)
-            label(self.level_0, self.bg_colour, self.txt_colour, "This is the first level")
+            label(self.level_0, self.bg_colour, self.txt_colour, """This is Level Zero""")
         elif level_num == 1:
             print("this is the first level")
             
@@ -331,6 +342,7 @@ class window: # This class is used to create the window of the programme
 
         # Create or show the appropriate content frame based on page_number
         frame_attr = f"content_frame_{self.page_number}"
+
         if hasattr(self, frame_attr):
             getattr(self, frame_attr).pack()
         else:
